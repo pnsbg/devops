e86f571343a651882dd53ed0cc83cce36a8b2724
Peter Tsonkov <ptsonkov@abv.bg>
2018-01-09 15:00:05 +0200

    exam completed :)

--- devopsExam/source/jenkins/jobs/doJobs.sh
+++ devopsExam/source/jenkins/jobs/doJobs.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-sleepsec="35"
+sleepsec="40"
 
 echo -e ">>> Create DB container..."
-java -jar /opt/jenkins-cli.jar -s http://jenkins:8080 -auth admin:admin build -s doDbContainer
+java -jar /opt/jenkins-cli.jar -s http://jhost.sulab.exam:8080 -auth admin:admin build -s doDbContainer
 
 echo -e ">>> sleep $sleepsec sec..."
 sleep $sleepsec
   
-echo -e ">>> Import DB container..."
-java -jar /opt/jenkins-cli.jar -s http://jenkins:8080 -auth admin:admin build -s importDbContainer
+echo -e ">>> Create PHP container..."
+java -jar /opt/jenkins-cli.jar -s http://jhost.sulab.exam:8080 -auth admin:admin build -s doPhpContainer
   
-echo -e ">>> Create WEB image..."
-java -jar /opt/jenkins-cli.jar -s http://jenkins:8080 -auth admin:admin build -s doWebImage
-  
-echo -e ">>> Create WEB container..."
-java -jar /opt/jenkins-cli.jar -s http://jenkins:8080 -auth admin:admin build -s doWebContainer
+echo -e ">>> Create NGINX container..."
+java -jar /opt/jenkins-cli.jar -s http://jhost.sulab.exam:8080 -auth admin:admin build -s doWebContainer
